69f790306bb83d5814d26ebbad20dc09d93ba8e9
hoaitx <tonghoai.tnn@gmail.com>
2019-03-01 10:27:46 +0700

    init repocms

---
+++ admin/exit.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ *
+ * @ Cydia Source Create
+ *
+ * @ Author: Tí Nhí Nhố (tonghoai)
+ *
+ * @ Vui lòng không chỉnh sửa bản quyền
+ *
+ */
+
+session_start();
+session_destroy();
+header('Location: index.php');
---
+++ incf/db.php
@@ -0,0 +1,9 @@
+<?php
+SESSION_START();
+/*** Thông tin Admin ***/
+$trang_useradmin = 'hoaitx';
+$trang_nickadmin = 'hoaitx';
+$trang_passadmin = '0dd6e6371d91d01632bd87276cf66531';
+
+/*** Trang chủ ***/
+$trang_url = 'http://localhost/repocms';
